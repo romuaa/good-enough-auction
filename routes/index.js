600c3412018490b5ef2fdb18050157168fe8362d
Eero Nevalainen <eero.nevalainen@eficode.com>
2016-10-14 19:59:29 +0300

    Ability to do a basic post of a bid

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,37 @@ const pg = require('pg');
 const path = require('path');
 const connectionString = process.env.DATABASE_URL || 'postgres://auction:auction@localhost:5432/auction';
 
-/* GET home page. */
+/* GET home page.  */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/bids', (req, res, next) => {
+    console.log(req);
+  
+    const results = [];
+    const batchid = "foobar"; // generate randomly
+    const data = {name: req.body.name, 
+		  email: req.body.email,
+		  value: parseInt(req.body.value),
+		  batchid: batchid};
+
+    pg.connect(connectionString, (err, client, done) => {
+	
+	if (err) {
+	    done();
+	    console.log(err);
+	    return res.status(500).json({success: false, data: err});
+	}
+
+	client.query('INSERT INTO bids (name, email, value, batchid) values ($1, $2, $3, $4)',
+		    [data.name, data.email, data.value, data.batchid]);
+
+	return res.status(200).json({success: true});
+	
+    });
+
+});
+
 
 module.exports = router;
