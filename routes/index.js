f4a2fd6c2a6370e3a0ceca24c16f2248f4b1d150
Eero Nevalainen <eero.nevalainen@eficode.com>
2016-10-14 17:52:10 +0300

    Updated imports according to tutorial

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+const path = require('path');
+const connectionString = process.env.DATABASE_URL || 'postgres://auction:auction@localhost:5432/auction';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
